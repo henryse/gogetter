d59a22a8aea47c6b85b696fce733e67cbee95551
Henry Seurer <henryse@gmail.com>
2022-05-11 10:22:04 -0700

    upgrading gogetter for new build system.

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/henryse/go-getter
+
+go 1.18
+
+require github.com/google/uuid v1.3.0 // indirect
--- gogetter.go
+++ gogetter.go
@@ -41,42 +41,48 @@ import (
 
 func main() {
 
-	var librariesLocation string
+	// Parse Command line.
+	//
+	var libraries string
+	var sources string
 
-	flag.StringVar(&librariesLocation, "libraries", "", "Directory location of the libraries.txt file OR the libraries file to use.")
-	boolPtr := flag.Bool("version", false, "a bool")
+	flag.StringVar(&libraries, "libraries", "", "Location of the libraries file")
+	flag.StringVar(&sources, "source", "", "Directory location of the go files to parse.")
+	update := flag.Bool("update", false, "Update libraries from sources")
+	install := flag.Bool("install", false, "GO GET libraries")
+	showVersion := flag.Bool("version", false, "Show version")
+	showHelp := flag.Bool("help", false, "Show Help")
 	flag.Parse()
 
-	if *boolPtr == true {
-		fmt.Println("Go Getter Version 1.0")
-	} else {
-		// If no parameters simply look for the libraries.txt file in the gogetter's directory
-		if len(librariesLocation) == 0 {
-			librariesLocation = path.Join(path.Dir(os.Args[0]), "libraries.txt")
-		} else {
-			// ok, if they just passed in a directory, then assume the file name is libraries.txt
-			if IsDirectory(librariesLocation) {
-				librariesLocation = path.Join(librariesLocation, "libraries.txt")
-			}
-		}
-
-		log.Printf("Installing libraries from: '%s'\n", librariesLocation)
-
-		file, err := os.Open(librariesLocation)
-		if err != nil {
-			log.Fatalf("Error: %s", err)
-		}
-		defer file.Close()
-
-		scanner := bufio.NewScanner(file)
-		for scanner.Scan() {
-			GoGet(scanner.Text())
-		}
-
-		if err := scanner.Err(); err != nil {
-			log.Fatal(err)
-		}
+	if *showVersion == true {
+		ShowVersion()
 	}
+
+	if *showHelp == true {
+		ShowHelp()
+	}
+
+	if *update == true {
+		UpdateLibraries(sources, libraries)
+	}
+
+	if *install == true {
+		InstallLibraries(libraries)
+	}
+}
+
+func ShowVersion() {
+	fmt.Println("go-getter Version 20220510.1")
+}
+
+func ShowHelp() {
+	flag.PrintDefaults()
+}
+
+func UpdateLibraries(sources string, libraries string) {
+
+	fmt.Println(sources)
+	fmt.Println(libraries)
 }
 
 func IsDirectory(path string) bool {
@@ -93,23 +99,54 @@ func IsDirectory(path string) bool {
 	return false
 }
 
-// Heart of the tool, simply execute the "go get" command for each line of the libraries.txt file
-func GoGet(library string) {
-	if library[0] != '#' {
-		log.Printf("go get %s\n", library)
+func InstallLibraries(libraries string) {
+	// If no parameters simply look for the libraries.txt file in the gogetter's directory
+	if len(libraries) == 0 {
+		libraries = path.Join(path.Dir(os.Args[0]), "libraries.txt")
+	} else {
+		// ok, if they just passed in a directory, then assume the file name is libraries.txt
+		if IsDirectory(libraries) {
+			libraries = path.Join(libraries, "libraries.txt")
+		}
+	}
 
-		cmd := exec.Command("go", "get", library)
-		stdout, err := cmd.Output()
+	log.Printf("Installing libraries from: '%s'\n", libraries)
 
+	file, err := os.Open(libraries)
+	if err != nil {
+		log.Fatalf("Error: %s", err)
+	}
+	defer func(file *os.File) {
+		err := file.Close()
 		if err != nil {
-			log.Fatal(err.Error())
-			return
+			fmt.Println(err)
 		}
+	}(file)
 
-		output := string(stdout)
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		library := scanner.Text()
 
-		if len(output) > 0 {
-			log.Println(output)
+		if library[0] != '#' {
+			log.Printf("go get %s\n", library)
+
+			cmd := exec.Command("go", "get", library)
+			stdout, err := cmd.Output()
+
+			if err != nil {
+				log.Fatal(err.Error())
+				return
+			}
+
+			output := string(stdout)
+
+			if len(output) > 0 {
+				log.Println(output)
+			}
 		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
 	}
 }
